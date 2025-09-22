d8616931f1cc6f13cd4afca57eaab87cb3c585c0
Huy Tran <dantra06141@edu.halmstad.se>
2025-09-22 11:03:35 +0200

    Added ListPy

---
+++ ListPy/ListPy.py
@@ -0,0 +1,46 @@
+class List:
+    def __init__(self, capacity=4):
+        """Skapar en ny lista med given startkapacitet."""
+        self._data = [None] * capacity   # "array" med defaultvärden
+        self._size = 0                   # antal faktiska element
+ 
+    def append(self, value):
+        """Lägg till ett värde sist i listan."""
+        # TODO: kolla om arrayen är full -> skapa ny, större array och kopiera
+        pass
+ 
+    def insert(self, index, value):
+        """Lägg in ett värde på en viss position."""
+        # TODO: flytta elementen åt höger, sätt in värdet på rätt plats
+        pass
+ 
+    def remove(self, value):
+        """Ta bort första förekomsten av ett värde."""
+        # TODO: hitta index för värdet, flytta elementen åt vänster
+        pass
+ 
+    def pop(self):
+        """Ta bort och returnera elementet på en viss position (eller sist)."""
+        pass
+ 
+    def get(self, index):
+        """Returnera värdet på en viss position."""
+        pass
+ 
+    def set(self, index, value):
+        """Ändra värdet på en viss position."""
+        pass
+ 
+    def size(self):
+        """Returnerar antal element."""
+        pass
+ 
+    def isEmpty(self):
+        """Returnerar True/False beroende på om listan är tom."""
+        pass
+ 
+    def __str__(self):
+        """Returnerar en strängrepresentation, t.ex. [1, 2, 3]."""
+    pass
+ 
+l = List()
