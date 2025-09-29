ec820925bb85b47d94685e15f6d2e37a1e680824
Huy Tran <dantra06141@edu.halmstad.se>
2025-09-29 11:31:20 +0200

    Started working on PyMethods.py

---
+++ PyMethods/PyMethods.py
@@ -0,0 +1,8 @@
+class Bok:
+    def __init__(self, titel, författare, utgivningsår):
+        self._titel = titel
+        self._författare = författare
+        self._utgivningsår = utgivningsår
+
+    def __str__(self):
+        pass
