600bfe97eca8ab3ab5bc5514a0882eea1abfba66
Huy Tran <dantra06141@edu.halmstad.se>
2025-10-17 14:22:06 +0200

    working on insert()

--- PyÖvn/LinkedList.py
+++ PyÖvn/LinkedList.py
@@ -19,7 +19,31 @@ class LinkedList:
             self._tail._next = new_node
             self._tail = new_node
 
-        self._length += 1    
+        self._length += 1
+
+    def prepend(self, value):
+        new_node = Node(value)
+        new_node._next = self._head
+        self._head = new_node
+
+        if self._head is None:
+            self._tail = new_node
+        
+        self._length += 1
+
+    def insert(self, index, value):
+        if index >= self._length:
+            self.append(value)
+            return
+        
+        if index <= 0:
+            self.prepend(value)
+            return
+
+        if index > 0:
+            new_node = Node(value)
+
+            return
 
     def isEmpty(self):
-        return self._length == 0
+        return self._head is None
