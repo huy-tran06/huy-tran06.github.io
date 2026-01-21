2441bf8188dabbf4b1bc0de1766deb48e8d1e1dd
Huy Tran <dantra06141@edu.halmstad.se>
2026-01-21 15:44:19 +0100

    började på  slutprojekt

---
+++ Mini-Axami/src/views/Login.vue
@@ -0,0 +1,20 @@
+<script>
+    import { ref } from "vue"
+
+
+    const email = ref("")
+
+    function login(){
+        console.log("Test login:", email.value)
+    }
+</script>
+
+<template>
+
+    <div>
+        <h1>Login</h1>
+        <input v-model="email" placeholder="Email"/>
+        <button @click="login">Test send</button>
+    </div>
+    
+</template>
