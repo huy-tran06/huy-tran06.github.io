66973a2352d250b09a7fe42c095b1d63ff094fc0
Huy Tran <dantra06141@edu.halmstad.se>
2026-02-17 17:54:36 +0100

    policies for units done

--- Mini-Axami/src/views/DashboardAdmin.vue
+++ Mini-Axami/src/views/DashboardAdmin.vue
@@ -1,5 +1,31 @@
+<script setup>
+import { supabase } from "../lib/supabase";
+
+async function testCreateUnit() {
+    console.log("Function triggered!")
+    const { data: userData } = await supabase.auth.getUser()
+    const user = userData.user
+
+    const { data, error } = await supabase
+    .from("units")
+    .insert({
+        name: "Test Unit",
+        description: "Temporary test",
+        owner_id: user.id
+    })
+
+  console.log("DATA:", data)
+  console.log("ERROR:", error)
+}
+
+</script>
+
 <template>
     <div>
         <h1>Admin Dashboard</h1>
     </div>
+
+    <button @click="testCreateUnit">
+        Create test unit
+    </button>
 </template>
--- Mini-Axami/src/views/DashboardWorker.vue
+++ Mini-Axami/src/views/DashboardWorker.vue
@@ -1,5 +1,30 @@
+<script setup>
+import { supabase } from "../lib/supabase";
+
+async function testCreateUnit() {
+    const { data: userData } = await supabase.auth.getUser()
+    const user = userData.user
+
+    const { data, error } = await supabase
+    .from("units")
+    .insert({
+      name: "Test Unit",
+      description: "Temporary test",
+      owner_id: user.id
+    })
+
+    console.log("DATA:", data)
+    console.log("ERROR:", error)
+}
+
+</script>
+
 <template>
     <div>
         <h1>Worker Dashboard</h1>
     </div>
+
+    <button @click="testCreateUnit">
+        Create test unit
+    </button>    
 </template>
